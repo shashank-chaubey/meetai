001d136cbc9ea583670c5670fccfd887aa326179
sh4sh4nk-9 <sashankchaubey2002@gmail.com>
2025-10-18 23:00:59 +0530

    19: meetings filters

--- src/modules/meetings/types.ts
+++ src/modules/meetings/types.ts
@@ -5,3 +5,10 @@
  export type MeetingGetMany = inferRouterOutputs<AppRouter>["meetings"]["getMany"]["items"];
  export type MeetingGetOne = inferRouterOutputs<AppRouter>["meetings"]["getOne"];
  
+ export enum MeetingStatus{
+    Upcoming = "upcoming",
+    Active = "active",
+    Completed = "completed",
+    Processing = "processing",
+    Cancelled = "cancelled",
+ }
